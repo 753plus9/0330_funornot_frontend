165042474d5f12f8d4da8a23ed24b61622cbcc36
sakai kazuto <kaz.xxxx1@gmail.com>
2025-04-08 23:54:54 +0900

    app.tsx削除

--- pages/for_daughter/app.tsx
+++
@@ -1,8 +0,0 @@
-// pages/_app.tsx
-import "@/styles/globals.css"; // ✅ パスはあってる？ styles フォルダがある？
-
-import type { AppProps } from 'next/app';
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
